be309361e1aee0bd03c7155f963a6e77e9c4a4a4
m-vanhoose <m-vanhoose@live.com>
2023-06-20 12:24:30 -0400

    resume added

--- src/components/pages/resume.js
+++ src/components/pages/resume.js
@@ -9,7 +9,56 @@ export default function Home() {
       <div class="container componentContent">
         <div class="row">
           <div class="col">
-            
+            <div class="resumeSection">Experience</div>
+            <div class="job">Valley Interior Systems, Columbus Ohio  -Carpenter</div>
+            <div class="jobTime">March 2022 - September 2022</div>
+              <ul class="desc">
+                <li class="resumeLi">Frame walls, hang drywall and ceiling grid/tile</li>
+                <li class="resumeLi">Apply fire seal and fire caulk as needed</li>
+                <li class="resumeLi">Help recieve material deliveries</li>
+                <li class="resumeLi">Organize supplies</li>
+                <li class="resumeLi">Operate lifts and various hand/power tools on a daily basis to complete aforementioned tasks</li>
+                <li class="resumeLi">Work and coordinate with other trades to move the job along with ease</li>
+                <li class="resumeLi">Clean up work areas and return any Valley supplies and material to proper storage locations</li>
+                <li class="resumeLi">Complete any other tasks received from foreman </li>
+              </ul>
+            <div class="job">City of Grove City, Grove City Ohio — Facility Maintenance Technician</div>
+            <div class="jobTime">August 2018 - March 2022</div>
+              <ul class="desc">
+                <li class="resumeLi">Tasked with surveying and maintaining city owned buildings including but not limited to; replacing bad light bulbs, drinking fountain filters, ceiling, VCT, and carpet tiles as well as replace furnace filters quarterly</li>
+                <li class="resumeLi">Primary interior painter for the city</li>
+                <li class="resumeLi">Work on major and minor building including  remodels which consisted of  tearing down and rebuilding walls with wood and metal studs, hanging and mudding drywall, hanging new ceiling tile grid, and working with concrete</li>
+                <li class="resumeLi">Completed checks of fire extinguishers and A.E.D.s as well as the condition of flags and flagpoles throughout the city</li>
+                <li class="resumeLi">Respond during emergency situations such as flooding and snow events</li>
+                <li class="resumeLi">Assist other departments as needed. For example helping the Parks Department level a field for a playground as well as constructing it.  A major project included the remodel of  one of our historical sites. Additional assistance included digging  post holes for batting cages.  Within the Streets Department, assisted framing and insulating a garage for storing several vehicles and large equipment as well as helping cleanup after city events</li>
+                <li class="resumeLi">Manage and resupply stock when needed</li>
+                <li class="resumeLi">Complete any other tasks given as the city saw fit</li>
+              </ul>
+              <div class="job">City of Grove City, Grove City Ohio — Custodian</div>
+            <div class="jobTime">August 2017 - August 2018</div>
+              <ul class="desc">
+                <li class="resumeLi">Removes trash, wipe down general use areas, clean bathrooms, dust, sweep, vacuum, and mop city owned buildings</li>
+                <li class="resumeLi">Operate and maintain floor scrubbers and buffers</li>
+                <li class="resumeLi">Manage and resupply stock when needed</li>
+                <li class="resumeLi">Report on any issues found in any building to supervisor</li>
+              </ul>
+            <div class="resumeSection">Education</div>
+            <div class="school">Columbus State Community College, Columbus Ohio — Associates of Science</div>
+            <div class="courses">Completed courses:</div>
+              <ul class="desc">
+                <li class="resumeLi">European History Spring 2018</li>
+                <li class="resumeLi">Intro to Psychology Spring 2018</li>
+                <li class="resumeLi">College Algebra Summer 2018</li>
+                <li class="resumeLi">Composition I Summer 2018</li>
+              </ul>
+              <div class="school">Columbus State Community College, Columbus Ohio — Skilled Trades Technology-Facilities Maintenance</div>
+            <div class="courses">Completed courses:</div>
+              <ul class="desc">
+                <li class="resumeLi">Math of Measurement Spring 2021</li>
+                <li class="resumeLi">Principles of Refrigeration Spring 2021</li>
+                <li class="resumeLi">Carpentry Fundamentals Autumn 2021</li>
+                <li class="resumeLi">Electrical Fundamentals Autumn 2021</li>
+              </ul>
           </div>
         </div>
       </div>
